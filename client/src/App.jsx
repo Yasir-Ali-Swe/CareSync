d0e64ad1e7c13c6cf90bf69a7a0fc9d12cc0e95c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-27 23:12:47 +0500

    Update App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "./components/ui/button.jsx";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default App;
