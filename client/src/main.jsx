0e85fc3774cd3a56aafb5050f827290f4b9e67fb
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-28 13:55:06 +0500

    Update main entry file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
+    <Toaster position="bottom-right" />
   </BrowserRouter>,
 );
-
-
