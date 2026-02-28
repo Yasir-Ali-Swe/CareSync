3afe2baa08d1c428842faa0c0ded92c40a52513c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-28 23:49:29 +0500

    Update App.jsx with recent changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "@/pages/auth/login";
 import Register from "@/pages/auth/register";
+import SendForgetPasswordRequest from "@/pages/auth/send-forget-password-request";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route
+        path="/send-forget-password-request"
+        element={<SendForgetPasswordRequest />}
+      />
     </Routes>
   );
 };
