2a81a4fd4d8d633fc756a5572e27aa7a01e2326a
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-03-02 17:07:13 +0500

    Update MessagesPgae page

--- client/src/pages/public/MessagesPgae.jsx
+++ client/src/pages/public/MessagesPgae.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import ConversationSidebar from "@/components/messages/ConversationSidebar";
+import MessageWindowHeader from "@/components/messages/MessageWindowHeader";
+import MessageInput from "@/components/messages/MessageInput";
+import MessageWindow from "@/components/messages/MessageWindow";
 
 const MessagesPgae = () => {
   return (
-    <div className="h-full flex flex-col justify-center items-center">
-      <h1 className="text-3xl text-primary font-bold">Welcome to CareSync</h1>
-      <p className="text-xl font-semibold">Messages Page</p>
+    <div className="h-screen flex">
+      <ConversationSidebar />
+      <div className="flex-1 flex flex-col ">
+        <MessageWindowHeader />
+        <MessageWindow className="flex-1" />
+        <MessageInput />
+      </div>
     </div>
   );
 };
