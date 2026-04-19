eb44e3733b52c12cf28e8a6a2125102085b5ce4f
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-04-19 20:59:25 +0500

    Update dashboard index redirect

--- client/src/components/dashboard/DashboardIndexRedirect.jsx
+++ client/src/components/dashboard/DashboardIndexRedirect.jsx
@@ -1,7 +1,9 @@
 import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { getDashboardHomeRoute } from "../../lib/DasboardRotes";
 
-const DashboardIndexRedirect = ({ role }) => {
+const DashboardIndexRedirect = () => {
+  const role = useSelector((state) => state.auth.role);
   return <Navigate to={getDashboardHomeRoute(role)} replace />;
 };
 
