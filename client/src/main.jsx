61b3dd82fb8f341c68cc57bd8e34f63f0938a2ed
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-04-19 20:59:31 +0500

    Update main entry file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,22 @@ import { BrowserRouter } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/provider/ThemeProvider";
+import { Provider } from "react-redux";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { store } from "@/store/store";
+import { queryClient } from "@/lib/queryClient";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <TooltipProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </TooltipProvider>
-    <Toaster position="bottom-right" />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <TooltipProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </TooltipProvider>
+        <Toaster position="bottom-right" />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </Provider>,
 );
