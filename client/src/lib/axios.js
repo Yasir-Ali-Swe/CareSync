af450427864e554af7d7e57e5f8489960ca50764
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-04-19 21:00:00 +0500

    Add axios config

---
+++ client/src/lib/axios.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+export const api = axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && originalRequest && !originalRequest._retry) {
+      originalRequest._retry = true;
+    }
+
+    return Promise.reject(error);
+  },
+);
