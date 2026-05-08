24686e030230c3efed50b83c97a405ff70340aa8
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-05-09 00:31:02 +0500

    feat: update admin routes

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
-import { getAdminStats, listUsers, updateUserStatus } from "../controllers/admin.controller.js";
+import {
+  getAdminProfile,
+  getAdminStats,
+  listUsers,
+  updateAdminProfile,
+  updateUserStatus,
+} from "../controllers/admin.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 import { allowRoles } from "../middlewares/role.middleware.js";
+import { upload } from "../middlewares/upload.middleware.js";
 import { ROLES } from "../utils/constants.js";
 
 const router = Router();
 
 router.use(protect, allowRoles(ROLES.ADMIN));
 
+router.get("/profile", getAdminProfile);
+router.patch("/profile", upload.single("avatar"), updateAdminProfile);
 router.get("/users", listUsers);
 router.patch("/users/:userId/status", updateUserStatus);
 router.get("/stats", getAdminStats);
