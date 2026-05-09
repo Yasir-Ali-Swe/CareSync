f208f2ba5d517aa4cc810a9877a11539b74ffd4c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-05-09 09:39:51 +0500

    feat: update auth routes with rate limiting

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -10,12 +10,16 @@ import {
   verifyEmail,
 } from "../controllers/auth.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
-import { authRateLimit } from "../middlewares/rateLimit.middleware.js";
+import {
+  authRateLimit,
+  loginAccountRateLimit,
+  loginIpRateLimit,
+} from "../middlewares/rateLimit.middleware.js";
 
 const router = Router();
 
 router.post("/register", authRateLimit, register);
-router.post("/login", authRateLimit, login);
+router.post("/login", loginIpRateLimit, loginAccountRateLimit, login);
 router.post("/refresh-token", refreshToken);
 router.post("/logout", logout);
 router.get("/verify-email/:token", verifyEmail);
